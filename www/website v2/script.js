33c340ba13a1f6d26e6c848dae6c3607902d78e4
vientrisi <georgetc62@gmail.com>
2022-10-07 00:34:44 +0100

    initial commit

---
+++ www/website v2/script.js
@@ -0,0 +1,11 @@
+<script>
+function scrollToProjects() {
+    var elem = document.getElementById("projects");
+    elem.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+function scrollToTop() {
+    var elem = document.getElementById("top");
+    elem.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
+</script>
